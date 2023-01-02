6690bc1a5b1e4cd734dbd217b9899e1278ee6458
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-01-02 06:37:10 +0530

    Chapter - 02 - Ignites our app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
     "h1",
     {
@@ -13,13 +16,29 @@ const heading = React.createElement(
     },
     "Heading 2"
   );
+
+  const para1 = React.createElement(
+    "p",
+    {
+      id: "title",
+    },
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis nulla eos magnam animi harum nobis omnis quasi nostrum odit iure?"
+  );
+
+  const para2 = React.createElement(
+    "p",
+    {
+      id: "title",
+    },
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero exercitationem saepe error eaque. Hic unde sequi fugiat doloribus voluptatum sint quaerat mollitia magnam quos ipsum accusantium excepturi laudantium, similique reiciendis?"
+  );
   
   const container = React.createElement(
     "div",
     {
       id: "container",
     },
-    [heading, heading2]
+    [heading, heading2, para1, para2]
   );
   
   console.log(heading);
