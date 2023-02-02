98a60e8ccecfd5c622843b3aaa90b992da71d927
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-02 08:39:19 +0530

    react.createElement and JSX

--- App.js
+++ App.js
@@ -1,41 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/*
+* Created A Server
+* HMR - Hot Module Replacement
+* File Watcher Algorithms - C++
+* BUNDLING
+* MINIFY
+* Cleaning our Code
+* Dev abd Production Build
+* Super Fast Build Algorithms
+* Image Optimization
+* Caching while developement
+* Comprehension
+* Compatible with older versions of browsers
+* HTTPS on dev
+* port number
+* Consistent Hashing algorithms
+* Zero config
+* Tree Shaking - Removing unwanted codes
+*
+*
+* Transitive dependencies 
+*/
+
+// React.createElement => Object => HTML(DOM)
+
 const heading = React.createElement(
   'h1',
   {
     id: 'title',
     key: 'h1',
   },
-  'Heading 1'
-);
-
-console.log(heading);
-
-const heading2 = React.createElement(
-  'h2',
-  {
-    id: 'title',
-    key: 'h2',
-  },
-  'Heading 2'
+  'Namaste React'
 );
 
 
-const container = React.createElement(
-  'div',
-  {
-    id: 'container',
-  },
-  [heading, heading2]
+//JSX => React.createElement => Object => HTML(DOM)
+
+// one line
+// const heading2 = <h1>Namaste React2</h1>
+
+// multiple lines
+const heading2 = (
+  <h1 id='title' key='h2'>
+    Namaste React 
+  </h1>
 );
 
-console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //passing a react element inside the root
 
 //async defer
-root.render(container);
+root.render(heading, heading2);
 
