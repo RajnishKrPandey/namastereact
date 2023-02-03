fe8251dce368761d350c8ba4f7f1a84477f782e1
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-03 12:56:03 +0530

    Renders React element inside functional component

--- App.js
+++ App.js
@@ -1,52 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// REACT ELEMENT
 const heading2 = (
   <h1 id="title" key="h2">
     Namaste React
   </h1>
 );
 
-// React Component
-// - Functional - NEW
-
-// IN ONE LINE
-const HeaderComponent1 = () => {
-  return <div><h1>Namaste React Functional Component</h1><h2>This is h2 tage</h2></div> 
-}
-
-// IN MULTIPLE LINES
-const HeaderComponent2 = () => {
+// REACT COMPONENT
+const HeaderComponent = () => {
   return (
     <div>
+      {heading2} 
       <h1>Namaste React Functional Component</h1>
       <h2>This is h2 tage</h2>
     </div>
   );
 };
 
-// WE CAN WRITE NORMAL FUNCTION ALSO
-const HeaderComponent3 = function () {
-  return (
-    <div>
-      <h1>Namaste React Functional Component</h1>
-      <h2>This is h2 tage</h2>
-    </div>
-  );
-};
-
-
-// IN MULTIPLE LINES WE WRITE MOSTLY THIS TYPES OF CODE
-const HeaderComponent4 = () => (
-  <div>
-      <h1>Namaste React Functional Component</h1>
-      <h2>This is h2 tage</h2>
-    </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //passing a react element inside the root
 
 //async defer
-root.render(heading, heading2);
+root.render(<HeaderComponent />);
