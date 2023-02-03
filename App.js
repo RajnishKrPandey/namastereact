05e759a813411f6bd0be841037583276968306b8
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-03 15:51:03 +0530

    Add Header

--- App.js
+++ App.js
@@ -1,40 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const title = (
-  <h1 id="title" key="h2">
-    This is just a React element which return JSX
-  </h1>
-)
+/**
+        Header
+           - Logo
+           - Nav Items (Right Side)
+           - Cart
+        Body
+           - Search bar
+           - RestaurantList
+               - Image
+               - Name
+               - Rating
+               - Cusines
+        Footer
+         - links
+         - Copyrights
+         
+       */
 
 const Title = () => (
-  <h1 id="title" key="h2">
-    This is a functional component by writing () => in React element we can create functional components 
-  </h1>
+  <a href="/">
+    <img
+      className="logo"
+      src="https://tse1.mm.bing.net/th?id=OIP._nsBEPa9cV8OUb_96KNxZAHaHa&pid=Api&P=0"
+      alt="logo"
+    />
+  </a>
 );
-
-const data = api.getData(); //  suppose api hacked and this return malicious code and this is known as xss attack
 
 const HeaderComponent = () => {
   return (
-    <div>
-      {data} <p>JSX is very secure. JSX takes care you can give it any data It will escape first. It will make sure your app is safe</p>
-      <p>JSX does sanitization</p>
-      {console.log('WE CAN WRITE ANY PEACE OF JAVASCRIPT CODE HERE ')}
-      {1+2}
-      {title}      <p> title IS JUST A NORMAL VARIABLE. Rendering React element </p>
-      <Title />    <p> Rendering Functional Component and This is known as COMPONENT COMPOSITION</p>
-      {Title()}    <p> We can also Render Functional Component like this because at the end It is just a JS function</p>
-      <h1>Namaste React Functional Component</h1>
-      <h2>This is h2 tage</h2>
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const AppLayout = () => {
+  return {};
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-//passing a react element inside the root
-
-//async defer
 root.render(<HeaderComponent />);
